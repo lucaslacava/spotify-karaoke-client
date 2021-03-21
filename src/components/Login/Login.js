e87259f3ca06c9e180476561d9316cc6ee189ee6
Lucas Lacava <lucas.lacava@ab-inbev.com>
2021-03-20 22:11:02 -0300

    changed redirect uri from spotify api

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 
 export const clientId = "8117b44d58db42d4827222362b23a282";
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=https://spotify-karaoke-server.herokuapp.com&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 export default function Login() {
   return (
